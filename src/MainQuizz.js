f7295100bb171c6ef7c4578300d8c04aad7e9904
WhiteTafal <givijf@gmail.com>
2022-11-05 20:07:48 +0400

    second upload

--- src/MainQuizz.js
+++ src/MainQuizz.js
@@ -24,7 +24,7 @@ export default function MainQuizz(props) {
     }
     
     return (
-        <main>
+        <main className="main-quizz-page">
             <div className="blob--yellow"></div>
             <div className="all-questions-container">                 
                 {renderQuestions()}
